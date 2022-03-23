b46c1cd37292446b88f6d8886f9b3fcdff0aafee
Peter Chapin <chapinp@acm.org>
2022-03-23 17:10:27 -0400

    Various updates to modernize the Scala and ANTLR versions in use.

--- bin/build-phoebe-lexer.bat
+++ bin/build-phoebe-lexer.bat
@@ -1,6 +1,10 @@
 @echo off
 
+REM This batch file is out of date, but it is being retained as a place holder in case it might
+REM prove useful in the future. Generating the Lexer should be done in IntelliJ using the ANTLR
+REM plugin.
+
 cd src\org\pchapin\phoebe
-java -cp ..\..\..\..\lib\antlr4-4.5.3.jar org.antlr.v4.Tool PhoebeLexer.g4
+java -cp ..\..\..\..\lib\antlr4-4.9.2.jar org.antlr.v4.Tool PhoebeLexer.g4
 cd ..\..\..\..
 
